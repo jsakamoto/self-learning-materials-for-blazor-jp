057ea95aeb6662f6de312fd7f27d9d53b8292bfb
jsakamoto <junichi.jp@gmail.com>
2018-04-13 16:05:45 +0900

    デバイス情報サービスの追加とDI経由での利用

---
+++ BlazorWOL.Client/DeviceService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using BlazorWOL.Shared;
+
+namespace BlazorWOL.Client
+{
+    public class DeviceService
+    {
+        private List<Device> Devices { get; } = new List<Device> {
+            new Device {Name = "Odin", MACAddress = "00:15:5D:52:CA:B6"},
+            new Device {Name = "Thor", MACAddress = "00:0C:29:30:7D:5D"},
+            new Device {Name = "Fenrir", MACAddress = "00:50:56:01:43:86"}
+        };
+
+        public IEnumerable<Device> GetDevices()
+        {
+            return Devices;
+        }
+    }
+}
