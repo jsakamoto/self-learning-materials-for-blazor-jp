9b0376c59274310f745113942487eb797fe9735d
jsakamoto <junichi.jp@gmail.com>
2019-08-19 19:53:14 +0900

    デバイス情報サービスの追加とDI経由での利用

--- Client/Program.cs
+++ Client/Program.cs
@@ -14,6 +14,7 @@ namespace BlazorWOL.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<DeviceService>();
 
             await builder.Build().RunAsync();
         }
