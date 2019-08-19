b668d87e374942a3bc40b7aa6a956521f7ff5e83
jsakamoto <junichi.jp@gmail.com>
2019-08-19 19:22:04 +0900

    プロジェクト新規作成

---
+++ Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorWOL.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
