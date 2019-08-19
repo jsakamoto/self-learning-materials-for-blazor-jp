0b45c24393b1b7dfd209efacee07c64bb403407a
jsakamoto <junichi.jp@gmail.com>
2019-08-19 19:54:51 +0900

    デバイス情報編集

--- Client/DeviceService.cs
+++ Client/DeviceService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BlazorWOL.Shared;
 
@@ -20,6 +22,21 @@ namespace BlazorWOL.Client
         public async Task AddDeviceAsync(Device device)
         {
             await Task.Run(() => Devices.Add(device));
+        }
+
+        public async Task<Device> GetDeviceAsync(Guid id)
+        {
+            return await Task.Run(() => Devices.FirstOrDefault(dev => dev.Id == id));
+        }
+
+        public async Task UpdateDeviceAsync(Guid id, Device device)
+        {
+            await Task.Run(() =>
+            {
+                var updateTarget = Devices.FirstOrDefault(dev => dev.Id == id);
+                updateTarget.Name = device.Name;
+                updateTarget.MACAddress = device.MACAddress;
+            });
         }
     }
 }
