700857c9775163b50aefc625b051af1eb4627a23
jsakamoto <junichi.jp@gmail.com>
2019-08-19 19:55:17 +0900

    サーバー側実装 - Web API を公開

---
+++ Server/DeviceStorage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BlazorWOL.Shared;
+using Newtonsoft.Json;
+
+namespace BlazorWOL.Server
+{
+    public class DeviceStorage
+    {
+        private string StoragePath { get; }
+
+        private List<Device> Devices { get; } = new List<Device>();
+
+        public DeviceStorage(string storagePath)
+        {
+            StoragePath = storagePath;
+            if (File.Exists(StoragePath))
+            {
+                var json = File.ReadAllText(StoragePath);
+                Devices.AddRange(JsonConvert.DeserializeObject<Device[]>(json));
+            }
+        }
+
+        public List<Device> GetDevices()
+        {
+            lock (this) return Devices;
+        }
+
+        public void AddDevice(Device device)
+        {
+            lock (this)
+            {
+                this.Devices.Add(device);
+                FlushToStorage();
+            }
+        }
+
+        public Device GetDevice(Guid id)
+        {
+            lock (this) return Devices.Find(dev => dev.Id == id);
+        }
+
+        public void UpdateDevice(Guid id, Device device)
+        {
+            lock (this)
+            {
+                var updateTo = GetDevice(id);
+                updateTo.Name = device.Name;
+                updateTo.MACAddress = device.MACAddress;
+                FlushToStorage();
+            }
+        }
+
+        public Device DeleteDevice(Guid id)
+        {
+            lock (this)
+            {
+                var device = GetDevice(id);
+                if (device != null) Devices.Remove(device);
+                FlushToStorage();
+                return device;
+            }
+        }
+
+        private void FlushToStorage()
+        {
+            lock (this)
+            {
+                var json = JsonConvert.SerializeObject(Devices);
+                File.WriteAllText(StoragePath, json);
+            }
+        }
+    }
+}
