5c086fdc919abb5a108d023ee7b4b93d7ef7e6d9
jsakamoto <junichi.jp@gmail.com>
2019-08-19 19:53:53 +0900

    入力チェックと正規化

--- Shared/Device.cs
+++ Shared/Device.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorWOL.Shared
 {
@@ -6,8 +7,12 @@ namespace BlazorWOL.Shared
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
+        [Required(ErrorMessage = "デバイス名を入力してください。")]
+        [StringLength(20, ErrorMessage = "デバイス名は20文字までです。")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "MACアドレスを入力してください。")]
+        [RegularExpression(@"(?i)^[\da-f]{2}((:|-)[\da-f]{2}){5}$", ErrorMessage = "MACアドレスの書式が正しくありません。")]
         public string MACAddress { get; set; }
     }
 }
