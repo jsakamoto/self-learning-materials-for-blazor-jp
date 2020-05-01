e54223920163a14c082dfecf032a1c07750621c1
jsakamoto <junichi.jp@gmail.com>
2020-05-01 18:20:20 +0900

    モデル (Deviceクラス) の実装

---
+++ Shared/Device.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorWOL.Shared
+{
+    public class Device
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string MACAddress { get; set; }
+    }
+}
