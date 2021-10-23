4909b5d3743dad2756a3e06ba5027bda6712c353
jsakamoto <junichi.jp@gmail.com>
2021-10-23 16:40:33 +0900

    モデル (Clockクラス) の実装

---
+++ Shared/Clock.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorWorldClock.Shared;
+
+public class Clock
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = "";
+
+    public string TimeZoneId { get; set; } = "";
+
+    public DateTime GetCurrentTime()
+    {
+        var timeZone = TimeZoneInfo.FindSystemTimeZoneById(this.TimeZoneId);
+        return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone);
+    }
+}
