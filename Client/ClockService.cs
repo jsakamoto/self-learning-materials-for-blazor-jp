9958ace1978f6acabf59ceb7e993569febfade3d
jsakamoto <junichi.jp@gmail.com>
2021-10-24 20:26:07 +0900

    時計情報サービスの追加とDI経由での利用

---
+++ Client/ClockService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorWorldClock.Shared;
+
+namespace BlazorWorldClock.Client;
+
+public class ClockService
+{
+    private List<Clock> Clocks { get; } = new()
+    {
+        new() { Name = "札幌", TimeZoneId = "Asia/Tokyo" },
+        new() { Name = "バンコク", TimeZoneId = "Asia/Bangkok" },
+        new() { Name = "シアトル", TimeZoneId = "America/Los_Angeles" },
+    };
+
+    public IEnumerable<Clock> GetClocks()
+    {
+        return this.Clocks;
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ClockService>();
 
 await builder.Build().RunAsync();
