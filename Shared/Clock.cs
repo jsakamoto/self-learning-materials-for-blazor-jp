ed02e3bf9c9753a598903ad9f81085a3e03b1a7c
jsakamoto <junichi.jp@gmail.com>
2021-10-25 21:49:52 +0900

    入力チェック

--- Shared/Clock.cs
+++ Shared/Clock.cs
@@ -1,11 +1,16 @@
-﻿namespace BlazorWorldClock.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWorldClock.Shared;
 
 public class Clock
 {
     public Guid Id { get; set; } = Guid.NewGuid();
 
+    [Required(ErrorMessage = "表示名を入力してください。")]
+    [StringLength(20, ErrorMessage = "表示名は20文字までです。")]
     public string Name { get; set; } = "";
 
+    [Required(ErrorMessage = "タイムゾーンを選択してください。")]
     public string TimeZoneId { get; set; } = "";
 
     public DateTime GetCurrentTime()
