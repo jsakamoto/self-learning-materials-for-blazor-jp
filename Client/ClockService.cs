db7ab3e3a7736d418bb5988347068b3df3f94274
jsakamoto <junichi.jp@gmail.com>
2026-09-14 22:24:02 +0900

    Step 8. 時計情報の取得・登録を行うサービスの実装 - DIの使用

---
+++ Client/ClockService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorWorldClock.Shared;
+
+namespace BlazorWorldClock.Client;
+
+public class ClockService
+{
+    private readonly List<Clock> _clocks = [
+        new() { Name = "札幌", TimeZoneId = "Asia/Tokyo" },
+        new() { Name = "バンコク", TimeZoneId = "Asia/Bangkok" },
+        new() { Name = "シアトル", TimeZoneId = "America/Los_Angeles" }
+    ];
+
+    public IEnumerable<Clock> GetClocks() => _clocks;
+}
