e5b47b983c7f60ac257178e5ff48c2f44386eb9e
jsakamoto <junichi.jp@gmail.com>
2026-09-14 22:41:08 +0900

    時計情報サービスのインターフェース分離

---
+++ Client/IClockService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorWorldClock.Shared;
+
+namespace BlazorWorldClock.Client;
+
+public interface IClockService
+{
+    ValueTask<IEnumerable<Clock>> GetClocksAsync();
+    ValueTask AddClockAsync(Clock clock);
+    ValueTask<Clock?> GetClockAsync(Guid id);
+    ValueTask UpdateClockAsync(Clock clock);
+    ValueTask DeleteClockAsync(Guid id);
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ClockService>();
+builder.Services.AddScoped<IClockService, ClockService>();
 
 await builder.Build().RunAsync();
